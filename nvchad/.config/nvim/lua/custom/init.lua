243601126a83118a2b564322fc3224deb7040b32
curve <heiyangs@gmail.com>
2023-08-19 10:32:41 +0800

    im switch

--- nvchad/.config/nvim/lua/custom/init.lua
+++ nvchad/.config/nvim/lua/custom/init.lua
@@ -5,3 +5,28 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+
+local opt = vim.opt
+local g = vim.g
+
+
+-- im-switch 
+-- The IME to invoke for managing input languages (macos, fcitx, ibus, xkb-switch)
+g.barbaric_ime = 'macos'
+
+-- The input method for Normal mode (as defined by `xkbswitch -g`, `ibus engine`, or `xkb-switch -p`)
+g.barbaric_default = 0
+
+-- The scope where alternate input methods persist (buffer, window, tab, global)
+g.barbaric_scope = 'buffer'
+
+-- Forget alternate input method after n seconds in Normal mode (disabled by default)
+-- Useful if you only need IM persistence for short bursts of active work.
+g.barbaric_timeout = -1
+
+-- The fcitx-remote binary (to distinguish between fcitx and fcitx5)
+-- g.barbaric_fcitx_cmd = 'xkbswitch'
+
+-- The xkb-switch library path (for Linux xkb-switch users ONLY)
+-- g.barbaric_libxkbswitch = os.getenv("HOME") .. '/.local/lib/libxkbswitch.so'
